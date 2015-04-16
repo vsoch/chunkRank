9e9785baf1c94087eda5976c15123779da2a4695
vsoch <vsochat@stanford.edu>
2015-04-16 15:36:38 -0700

    adding cutoff to algorithm

--- R/chunkRank.R
+++ R/chunkRank.R
@@ -1,8 +1,10 @@
 #' Chunk Indifferent Ranking Algorithm
 #'
-#' Calculates an accuracy for a ranked list of binary, monotonic groupings, "chunks" for which an ordering of the groups is desired, but no order of the elelmens within the groups is specified.  For example, if doing a query to return a list of results, we may want the first N to be of a particular type, but we don't care about the ordering within those first N. We may next want a group of type G, followed by R, and for both we are indifferent about the rankings within the groups. An accuracy is returned for each group, and it is up to the user to decide which to use, weighting, etc.
-#' @param lookup A vector of ints, with each int corresponding to the actual group assignment, and names(lookup) corresponding the unique id. This variable will be sorted to derive the "ideal" gold standard [eg, 1 1 1 1 2 2 2 2... N]
-#' @param predicted The predicted ordering, all labels in predicted must overlap with labels in chunk, and lengths must be equal 
+#' Calculates an accuracy for a ranked list of binary, monotonic groupings, "chunks" for which an ordering of the groups is desired, but no order of the elements within the groups is specified.  For example, if doing a query to return a list of results, we may want the first N to be of a particular type, but we don't care about the ordering within those first N. We may next want a group of type G, followed by R, and for both we are indifferent about the rankings within the groups. An accuracy is returned for each group, and it is up to the user to decide which to use, weighting, etc.
+#' @param lookup A vector of ints, with each int corresponding to the actual group assignment, and names(lookup) corresponding the unique id. If gold standard "ideal" is not provided, this variable will be sorted to derive the "ideal" gold standard [eg, 1 1 1 1 2 2 2 2... N]
+#' @param predicted The predicted ordering, all labels in predicted must be present in lookup, and lengths must be equal
+#' @param ideal The ideal gold standard ordering, increasing [eg, 1 1 1 1 2 2 2 2.. N]  
+#' @param cutoff The cutoff position for giving partial accuracy. This can be the number of images returned in the first pagination of an image search, the size of the first group (no partial accuracy given), or the length of the list (partial accuracy given for all) [default = 100]
 #' @keywords ranking, chunk indifferent ranking, sorting
 #' @export
 #' @examples
@@ -13,16 +15,20 @@
 #' 
 #' ### The predicted order
 #' predicted = sample(names(lookup),length(lookup)) 
-#' results = chunkRank(lookup,ideal)
+#' results = chunkRank(lookup,predicted)
 
-chunkRank = function(lookup,predicted){
+chunkRank = function(lookup,predicted,ideal,cutoff=100){
  
     # We will return results in a list
     results = list()
  
-    # ideal is the sorted lookup
-    ideal = as.numeric(sort(lookup,decreasing=FALSE))
- 
+    if (missing(ideal)){
+      # ideal is the sorted lookup
+      ideal = as.numeric(sort(lookup,decreasing=FALSE))
+    } else {
+      ideal = sort(ideal,decreasing=FALSE)
+    }
+
     # Check that all predicted names are in lookup
     if (any(!predicted %in% names(lookup))) {
         missing = names(lookup)[-which(predicted %in% names(lookup))]
@@ -30,7 +36,7 @@ chunkRank = function(lookup,predicted){
     }
 
     # Check that counts are the same for gold standard and lookup
-    if (!all(as.numeric(table(chunks)) == as.numeric(table(lookup)))){
+    if (!all(as.numeric(table(ideal)) == as.numeric(table(lookup)))){
         stop("Counts and group identifiers of labels in chunks and lookup must be equivalent!")
     }
   
@@ -79,7 +85,18 @@ chunkRank = function(lookup,predicted){
       # For the wrong predictions, we need to know how far off we were
       actual_indices = which(lookup==group)
       actual_indices = actual_indices[-which(names(actual_indices) %in% corr)]
+
+      # Only give partial accuracy up to the cutoff
+      if (any(actual_indices>cutoff)){
+        actual_indices = actual_indices[-which(actual_indices>cutoff)]
+      }
       
+      # We will weight accuracy contribution based on distance from worst
+      # case position, as well as position in the list.
+      #weights = rev(seq(1,length(predicted)) / length(predicted))
+      # I think what we want to do is weight accuracy for each based on absolute position in the list
+      # Can we set a "cut point" and subtract accuracy beyond that? How can we be sure it will even out?
+
       # Case 1: If we are at the first group, we will measure from the last position of the group 1 label
       # [1,1,1<--last ok position,2,2,2,2<--worst case]
       if (group==1){
